85d325e39932053b08d83fc3a0bbaf2f5ab65c7f
AlessioFelicioni <93184410+AlessioFeli@users.noreply.github.com>
2022-05-31 18:11:56 +0200

    Update and rename ClientjavaGio.java to Clientjava.java

--- Clientjava.java
+++ Clientjava.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package serverjavagio;
+package serverjava;
 import java.net.*;
 import java.io.*;
 import java.util.logging.Level;
@@ -13,7 +13,7 @@ import java.util.logging.Logger;
  *
  * @author Studente
  */
-public class ClientjavaGio {
+public class Clientjava {
     
         DataOutputStream out;
           BufferedReader in;
